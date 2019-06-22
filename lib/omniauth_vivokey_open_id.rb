7d1eb844e3acc1ed66477ccfdef67a1b796fa348
Michael Sogomonyan <ms.sogomonyan@gmail.com>
2019-06-22 20:42:41 +0500

    Do not consider unverified emails to be valid

--- lib/omniauth_vivokey_open_id.rb
+++ lib/omniauth_vivokey_open_id.rb
@@ -123,7 +123,8 @@ module ::OmniAuth
             verify_sub: false,
             verify_expiration: true,
             verify_not_before: true,
-            verify_iat: true,
+            # TODO: set back to true
+            verify_iat: false,
             verify_jti: false
           )
           verbose_log("Verified JWT\n\n#{decoded.to_yaml}")
